f7eb3ccd4a8647dd9384ab3681932df1d4a53dd3
Conrad Heinrich Carl <conrad_heinrich.carl@mailbox.tu-dresden.de>
2023-01-21 19:32:49 +0100

    better debug print

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 mod opcode;
 mod lexer;
 mod parser;
-mod stack_machine;
+// mod stack_machine;
 
-use std::fs::{self, read_to_string};
+use std::fs::{read_to_string};
 
 fn main() {
     // read file input as text and print it
     let input = read_to_string("test.f").unwrap();
 
-    let mut tokens = lexer::Lexer::lex(&input);
+    let tokens = lexer::Lexer::lex(&input);
+    let ast = parser::Parser::new(&tokens).parse();
     
-    println!("{:?}", tokens);
+    println!("{:#?}", ast);
 }
