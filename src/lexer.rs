37497ee52cd5b68dba14b831e856a3544c8b67ac
Conrad Heinrich Carl <conrad_heinrich.carl@mailbox.tu-dresden.de>
2023-01-21 15:44:41 +0100

    add basic lexer and test.f

---
+++ src/lexer.rs
@@ -0,0 +1,118 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
+#[derive(Debug, PartialEq)]
+pub(crate) enum Token {
+    Number(i32),
+    StringLiteral(String),
+    Identifier(String),
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Emit,
+    Dup,
+    Swap,
+    Drop,
+    Lt,
+    Gt,
+    Eq,
+}
+
+pub(crate) struct Lexer<'a> {
+    chars: Peekable<Chars<'a>>,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(input: &'a str) -> Lexer<'a> {
+        Lexer {
+            chars: input.chars().peekable(),
+        }
+    }
+
+    pub fn lex(input: &'a str) -> Vec<Token> {
+        let mut lexer = Lexer::new(input);
+        let mut tokens = vec![];
+        while let Some(token) = lexer.next_token() {
+            tokens.push(token);
+        }
+        tokens
+    }
+
+    fn next_token(&mut self) -> Option<Token> {
+        while let Some(&c) = self.chars.peek() {
+            if c.is_whitespace() {
+                self.chars.next();
+                continue;
+            }
+
+            if c.is_numeric() {
+                return Some(self.number());
+            }
+
+            if c.is_ascii_alphabetic() {
+                return Some(self.identifier());
+            }
+
+            if c == '"' {
+                return Some(self.string());
+            }
+
+            // parse single character tokens
+            self.chars.next();
+            match c {
+                '+' => return Some(Token::Add),
+                '-' => return Some(Token::Sub),
+                '*' => return Some(Token::Mul),
+                '/' => return Some(Token::Div),
+                '.' => return Some(Token::Emit),
+                _ => return None,
+            }
+        }
+
+        None
+    }
+
+    fn number(&mut self) -> Token {
+        let mut number = String::new();
+        while let Some(&c) = self.chars.peek() {
+            if c.is_numeric() {
+                number.push(c);
+                self.chars.next();
+            } else {
+                break;
+            }
+        }
+
+        Token::Number(number.parse().unwrap())
+    }
+
+    fn string(&mut self) -> Token {
+        self.chars.next(); // consume the initial "
+        let mut string = String::new();
+        while let Some(&c) = self.chars.peek() {
+            if c == '"' {
+                self.chars.next(); // consume the final "
+                return Token::StringLiteral(string);
+            } else {
+                string.push(c);
+                self.chars.next();
+            }
+        }
+        Token::StringLiteral(string)
+    }
+
+    fn identifier(&mut self) -> Token {
+        let mut identifier = String::new();
+        while let Some(&c) = self.chars.peek() {
+            if c.is_ascii_alphanumeric() || c == '_' {
+                identifier.push(c);
+                self.chars.next();
+            } else {
+                break;
+            }
+        }
+        Token::Identifier(identifier)
+    }
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod opcode;
+mod lexer;
+
+use std::fs::{self, read_to_string};
 
 fn main() {
-    println!("Hello, world!");
+    // read file input as text and print it
+    let input = read_to_string("test.f").unwrap();
+
+    let mut tokens = lexer::Lexer::lex(&input);
+    
+    println!("{:?}", tokens);
 }
