7238c0fea113c3e1360d4eea60296bbfabc002bc
Conrad Heinrich Carl <conrad_heinrich.carl@mailbox.tu-dresden.de>
2023-01-26 20:13:59 +0100

    added some tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,20 +1,46 @@
 #[cfg(test)]
 mod tests {
-    use crate::{stacky::Stacky};
-
-    
+    use crate::stacky::Stacky;
 
     #[test]
     fn add() {
         let input = "1 2 + .";
+        let mut output: Vec<u8> = Vec::new();
         let mut stacky = Stacky::new();
         let tokens = stacky.lex(&input);
         let ast = stacky.parse(&tokens);
-        stacky.run(&ast);
+        stacky.run(&ast, &mut output);
+
+        let _s = match String::from_utf8(output) {
+            Ok(v) => assert!(v == "3"),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
     }
 
     #[test]
     fn word() {
-        let input = "";
+        let input = ":Test 1 2 + . ; Test";
+        let mut output: Vec<u8> = Vec::new();
+        let mut stacky = Stacky::new();
+        let tokens = stacky.lex(&input);
+        let ast = stacky.parse(&tokens);
+        stacky.run(&ast, &mut output);
+
+        let _s = match String::from_utf8(output) {
+            Ok(v) => assert!(v == "3"),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
+
+    #[test]
+    #[should_panic(expected = "Word not found")]
+    fn word_not_defined() {
+        let input = ":Test 1 2 + . ; Testl";
+        let mut output: Vec<u8> = Vec::new();
+        let mut stacky = Stacky::new();
+        let tokens = stacky.lex(&input);
+        let ast = stacky.parse(&tokens);
+        stacky.run(&ast, &mut output);
+
     }
 }
