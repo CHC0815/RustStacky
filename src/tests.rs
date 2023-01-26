dac2c648c3f6237909ce3c12ad65d75c430fc6d2
Conrad Heinrich Carl <conrad_heinrich.carl@mailbox.tu-dresden.de>
2023-01-26 20:59:41 +0100

    gte tests

--- src/tests.rs
+++ src/tests.rs
@@ -133,6 +133,49 @@ mod tests {
         };
     }
 
-    
+    #[test]
+    fn gte_equals() {
+        let input = "2 2 >= .";
+        let mut output: Vec<u8> = Vec::new();
+        let mut stacky = Stacky::new();
+        let tokens = stacky.lex(&input);
+        let ast = stacky.parse(&tokens);
+        stacky.run(&ast, &mut output);
+
+        let _s = match String::from_utf8(output) {
+            Ok(v) => assert!(v == "1"),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
+
+    #[test]
+    fn gte_greater() {
+        let input = "3 2 >= .";
+        let mut output: Vec<u8> = Vec::new();
+        let mut stacky = Stacky::new();
+        let tokens = stacky.lex(&input);
+        let ast = stacky.parse(&tokens);
+        stacky.run(&ast, &mut output);
+
+        let _s = match String::from_utf8(output) {
+            Ok(v) => assert!(v == "1"),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
+
+    #[test]
+    fn gte_false() {
+        let input = "1 2 >= .";
+        let mut output: Vec<u8> = Vec::new();
+        let mut stacky = Stacky::new();
+        let tokens = stacky.lex(&input);
+        let ast = stacky.parse(&tokens);
+        stacky.run(&ast, &mut output);
+
+        let _s = match String::from_utf8(output) {
+            Ok(v) => assert!(v == "0"),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
 
 }
