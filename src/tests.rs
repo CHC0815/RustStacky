2241ebd4735210a5c6f67c926fa88cffd1d27715
Conrad Heinrich Carl <conrad_heinrich.carl@mailbox.tu-dresden.de>
2023-01-26 20:59:20 +0100

    lt tests

--- src/tests.rs
+++ src/tests.rs
@@ -73,6 +73,36 @@ mod tests {
         };
     }
 
+    #[test]
+    fn lt() {
+        let input = "1 2 < .";
+        let mut output: Vec<u8> = Vec::new();
+        let mut stacky = Stacky::new();
+        let tokens = stacky.lex(&input);
+        let ast = stacky.parse(&tokens);
+        stacky.run(&ast, &mut output);
 
+        let _s = match String::from_utf8(output) {
+            Ok(v) => assert!(v == "1"),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
+
+    #[test]
+    fn lt_false() {
+        let input = "2 2 < .";
+        let mut output: Vec<u8> = Vec::new();
+        let mut stacky = Stacky::new();
+        let tokens = stacky.lex(&input);
+        let ast = stacky.parse(&tokens);
+        stacky.run(&ast, &mut output);
+
+        let _s = match String::from_utf8(output) {
+            Ok(v) => assert!(v == "0"),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
+
+    
 
 }
