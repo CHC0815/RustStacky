f73eab9ba4bfc37bf127959bc86aa2d9b426618d
Conrad Heinrich Carl <conrad_heinrich.carl@mailbox.tu-dresden.de>
2023-01-27 13:38:39 +0100

    test dup

--- src/tests.rs
+++ src/tests.rs
@@ -293,4 +293,19 @@ mod tests {
         let ast = stacky.parse(&tokens);
         stacky.run(&ast, &mut output);
     }
+
+    #[test]
+    fn dup() {
+        let input = "2 DUP + .";
+        let mut output: Vec<u8> = Vec::new();
+        let mut stacky = Stacky::new();
+        let tokens = stacky.lex(&input);
+        let ast = stacky.parse(&tokens);
+        stacky.run(&ast, &mut output);
+
+        let _s = match String::from_utf8(output) {
+            Ok(v) => assert!(v == "4"),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
 }
