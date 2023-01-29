7edff516365356bb87437596584e61128ff1e5d4
Conrad Heinrich Carl <conrad_heinrich.carl@mailbox.tu-dresden.de>
2023-01-29 10:53:06 +0100

    added some loop tests

--- src/tests.rs
+++ src/tests.rs
@@ -337,4 +337,34 @@ mod tests {
             Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
         };
     }
+
+    #[test]
+    fn loop_test() {
+        let input = "10 0 DO 1 . LOOP";
+        let mut output: Vec<u8> = Vec::new();
+        let mut stacky = Stacky::new();
+        let tokens = stacky.lex(&input);
+        let ast = stacky.parse(&tokens);
+        stacky.run(&ast, &mut output);
+
+        let _s = match String::from_utf8(output) {
+            Ok(v) => assert!(v == "1111111111"),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
+
+    #[test]
+    fn loop_no_iteration() {
+        let input = "2 2 DO 1 . LOOP";
+        let mut output: Vec<u8> = Vec::new();
+        let mut stacky = Stacky::new();
+        let tokens = stacky.lex(&input);
+        let ast = stacky.parse(&tokens);
+        stacky.run(&ast, &mut output);
+
+        let _s = match String::from_utf8(output) {
+            Ok(v) => assert!(v == ""),
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+    }
 }
